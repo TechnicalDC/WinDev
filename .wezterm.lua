c6e9cb5c7ed8d7018f96017f569d9fe2836b9d93
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2023-12-21 00:17:38 +0530

    Add files via upload

--- .wezterm.lua
+++ .wezterm.lua
@@ -33,6 +33,45 @@ local colors = {
 	diff_text = "#2c5372",
 }
 
+local direction_keys = {
+	Left = 'h',
+	Down = 'j',
+	Up = 'k',
+	Right = 'l',
+	-- reverse lookup
+	h = 'Left',
+	j = 'Down',
+	k = 'Up',
+	l = 'Right',
+}
+
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == 'true'
+end
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+				}, pane)
+			else
+				if resize_or_move == 'resize' then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 wezterm.on('gui-startup', function()
 	local tab, pane, window = mux.spawn_window({})
 	window:gui_window():maximize()
@@ -162,6 +201,7 @@ config.show_update_window = true
 config.quote_dropped_files = "WindowsAlwaysQuoted"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = false
 config.tab_max_width = 30
 config.disable_default_key_bindings = true
 config.disable_default_mouse_bindings = false
@@ -229,7 +269,7 @@ config.colors = {
 			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
 			-- label shown for this tab.
 			-- The default is "Normal"
-			intensity = 'Normal',
+			intensity = 'Bold',
 
 			-- Specify whether you want "None", "Single" or "Double" underline for
 			-- label shown for this tab.
@@ -299,10 +339,20 @@ config.keys = {
 	{ action = action.ActivateTabRelative(-1)								, mods =	'CTRL|SHIFT', key =   'Tab' },
 	{ action = action.QuickSelect												, mods =	'CTRL|SHIFT', key = 'Space' },
 	{ action = action.ActivateCommandPalette								, mods =	'CTRL|SHIFT', key =		':' },
-	{ action = action.AdjustPaneSize { 'Left', resize_step }			, mods =	  'CTRL|ALT', key =		'h' },
-	{ action = action.AdjustPaneSize { 'Right', resize_step } 		, mods =	  'CTRL|ALT', key =		'l' },
-	{ action = action.AdjustPaneSize { 'Down', resize_step }	 		, mods =	  'CTRL|ALT', key =		'j' },
-	{ action = action.AdjustPaneSize { 'Up', resize_step }	 		, mods =	  'CTRL|ALT', key =		'k' },
+	-- move between split panes
+	split_nav('move', 'h'),
+	split_nav('move', 'j'),
+	split_nav('move', 'k'),
+	split_nav('move', 'l'),
+	-- resize panes
+	split_nav('resize', 'h'),
+	split_nav('resize', 'j'),
+	split_nav('resize', 'k'),
+	split_nav('resize', 'l'),
+	-- { action = action.AdjustPaneSize { 'Left', resize_step }			, mods =	       'ALT', key =		'h' },
+	-- { action = action.AdjustPaneSize { 'Right', resize_step } 		, mods =	       'ALT', key =		'l' },
+	-- { action = action.AdjustPaneSize { 'Down', resize_step }	 		, mods =	       'ALT', key =		'j' },
+	-- { action = action.AdjustPaneSize { 'Up', resize_step }	 		, mods =	       'ALT', key =		'k' },
 	{ action = action.PaneSelect { mode = "Activate"}					, mods = 'CTRL|SHIFT', key =     's' },
 }
 
